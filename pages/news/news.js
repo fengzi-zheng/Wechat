59e89de37baf604df696a9a957f48e879557d48c
fengzi <zheng-wenfeng@hotmail.com>
2020-09-27 23:39:38 +0800

    第一次提交初始化项目文件

---
+++ pages/news/news.js
@@ -0,0 +1,85 @@
+// pages/news/news.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    news:[
+      {
+        "title":"华为p40Pro换外屏，分离下外屏傻眼了，金刚丝分进触摸，亏一千多",
+        "url":"http://p6-tt-ipv6.byteimg.com/img/tos-cn-i-0004/b34ef5072aa34d738a1f1368ef56fed1~tplv-tt-cs0:640:360.jpg",
+        "sour":"凤凰新闻网",
+        "time":"1小时前"
+      },
+      {
+        "title":"容量差不多的冰箱重量竞然差一半，资深销售现场教你买冰箱",
+        "url":"http://p6-tt-ipv6.byteimg.com/img/tos-cn-i-0004/2cd18094891149cfb45ec275215326e8~tplv-tt-cs0:640:360.jpg",
+        "sour":"凤凰新闻网",
+        "time":"2小时前"
+      },
+      {
+        "title":"电动车增加一块电池能提升动力拉长里程，使用这个方法，立马见效",
+        "url":"http://p1-tt-ipv6.byteimg.com/img/tos-cn-i-0000/4570721fb50a44e8b3b67b471b1b7f9e~tplv-tt-cs0:640:360.jpg",
+        "sour":"凤凰新闻网",
+        "time":"3小时前"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
